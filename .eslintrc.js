fa700fa6b61f5f95e47334b301586fe1e68fcaae
Nazmul Alam <nalam@fexco.com>
2021-01-30 16:54:34 +0000

    added initial project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    mocha: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  }
+}
